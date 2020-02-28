0d44964b14fadeb50ec90b2f33e81d9ab250b46f
Bulat <garifb@gmail.com>
2020-02-28 10:03:33 +0300

    book

---
+++ index.js
@@ -0,0 +1,23 @@
+const form = document.getElementsByName("finder_form")[0]
+const date = document.getElementsByName("finder_start_date")[0]
+const apiUrl = "/news/date/list/"
+
+let searchUrl = ""
+
+// --------------- Events
+document.addEventListener("DOMContentLoaded", () => {
+    if (!form) return
+    date.addEventListener('change', onFormChange);
+    form.addEventListener('submit', onFormSumbit);
+})
+
+function onFormChange() {
+  searchUrl = apiUrl + date.value;
+}
+
+function onFormSumbit(evt) {
+    evt && evt.preventDefault && evt.preventDefault();
+    if (form.checkValidity()) {
+      window.location.href = searchUrl;
+    }
+}
